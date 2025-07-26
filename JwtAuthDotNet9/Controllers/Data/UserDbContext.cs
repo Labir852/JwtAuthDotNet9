099e5d547db57d7c98d91280c25ee72c41b6462e
Tanvir Anjum Labir <tanvir.labir@gmail.com>
2025-07-26 20:06:22 +0600

    all

---
+++ JwtAuthDotNet9/Controllers/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JwtAuthDotNet9.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotNet9.Controllers.Data
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
